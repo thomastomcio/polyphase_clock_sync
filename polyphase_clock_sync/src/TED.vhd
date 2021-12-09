314e7e9528895ccd89e376b87b2dc9d611497bd0
Tomasz <thomastomcio@gmail.com>
2021-12-09 17:28:50 +0100

    Trying to convert real vars to integer

--- polyphase_clock_sync/src/TED.vhd
+++ polyphase_clock_sync/src/TED.vhd
@@ -58,9 +58,15 @@ architecture TED_arch of TED is
 --  constant DENOM : real := PHASE_DETECOR_GAIN*(1 + 2*DAMPING_FACTOR*LOOP_BW + LOOP_BW*LOOP_BW); -- TODO: ustawiæ odpowiednie typy
 --	constant K1 : real := (4*DAMPING_FACTOR*LOOP_BW)/DENOM; -- alpha
 --	constant K2 : real := (4*LOOP_BW*LOOP_BW)/DENOM;		-- beta	
-	constant K1 : real := 0.0110017712591052; --0.162525308786227;	   
-	constant K2 : real := 9.77243613962953e-05; --0.014436477216089;
-	
+--	constant K1 : real := 0.0110017712591052; --0.162525308786227;	   
+--	constant K2 : real := 9.77243613962953e-05; --0.014436477216089;	
+
+
+	constant scale : integer := 2 ** 10;
+	constant K1 : integer := integer(real(scale)*0.0110017712591052); --0.162525308786227;	   
+	constant K2 : integer := integer(real(scale)*9.77243613962953e-05); --0.014436477216089;  
+	constant licznik : integer := scale*1024*1024;
+
 	--signal error : real := 0.0; -- signed(dfilter_din'range) := (others => '0');
 	--signal f_index_sig : std_logic_vector(integer(ceil(log2(real(CHANNELS))))-1 downto 0) := (others => '0');	-- TODO: ustaliæ typy
 	signal f_index_sig : integer := 0;	-- TODO: ustaliæ typy
@@ -77,33 +83,34 @@ architecture TED_arch of TED is
 
 begin
 process(arestn, clk)					  
-	----variable sign : real range -1.0 to 1.0 := 1.0;	-- sprawdziæ czy nie da siê na dwóch wartoœciach (-1, 1) 
-	variable temp : real := 0.0;
+--	variable sign : real range -1.0 to 1.0 := 1.0;	-- sprawdziæ czy nie da siê na dwóch wartoœciach (-1, 1) 
+--	variable temp : integer := 0;
 --	variable actual_f : signed(AXIS_DATA_WIDTH-1 downto 0) := (others => '0');  
 --	variable actual_df : signed(AXIS_DATA_WIDTH-1 downto 0) := (others => '0');		
-	variable error : real := 0.0;
+	variable error : integer := 0;
 --	variable prev_error : real := 0.0;
-	variable vp : real := 0.0;
-	variable  vi : real := 0.0;
-	variable  v : real := 0.0;
-	variable  W : real := 0.0;	
-	variable  CNT : real := 1.0; 	--modulo 1 counter 
+	variable vp : integer := 0;
+	variable  vi : integer := 0;
+	variable  v : integer := 0;
+	variable  W : integer := 0;	
+	variable  CNT : integer := licznik; 	--modulo 1 counter 
+	variable vi_add : integer := 0;
 begin
 	if (arestn = '0') then	
 		f_index_sig <= 0; -- (others => '0');
 		underflow <= '0';
-		vp := 0.0;
-		vi := 0.0;
-		v := 0.0;
-		W := 0.0;
-		CNT := 1.0;
+		vp := 0;
+		vi := 0;
+		v := 0;
+		W := 0;
+		CNT := licznik;
 	elsif (rising_edge(clk)) then
 		if (in_valid = '1') then
 		
 			if filter_din(filter_din'left) = '1' then 	-- determine sign of matched filter output																 
-				error := (-1.0)*real(to_integer(dfilter_din))/(1024.0*1024.0);  -- integer(unsigned(f_index)))  
+				error := ((-1)*to_integer(dfilter_din));  -- integer(unsigned(f_index)))  
 			else
-				error := real(to_integer(dfilter_din))/(1024.0*1024.0);  -- integer(unsigned(f_index)))  
+				error := (to_integer(dfilter_din));  -- integer(unsigned(f_index)))  
 			end if;
 			
 			-- error 
@@ -112,17 +119,19 @@ begin
 			
 			-- loop filter;
 			vp := K1*error;
-			vi := vi + K2*error;
+			
+			vi_add := K2*error;
+			
+			vi := vi + vi_add; 
 			v := vp + vi;
-			W := 1.0/real(SAMPLES_PER_SYMBOL) + v; -- update every SAMPLES_PER_SYMBOL in closed loop
+			W := licznik/SAMPLES_PER_SYMBOL + v; -- update every SAMPLES_PER_SYMBOL in closed loop
 		
 			-- counter														   
-			CNT := CNT - W;
-			temp := real(SAMPLES_PER_SYMBOL*OVERSAMPLING_RATE)*abs(CNT) mod real(OVERSAMPLING_RATE);
+			CNT := CNT - W; 
 			
-			if (CNT < 0.0) then
-				f_index_sig <= integer(floor(temp));
-				CNT := 1.0 + CNT;
+			if (CNT < 0) then
+				f_index_sig <= ((SAMPLES_PER_SYMBOL*OVERSAMPLING_RATE*abs(CNT)/scale)) mod (OVERSAMPLING_RATE);
+				CNT := licznik + CNT;
 				underflow <= '1';
 			else
 				underflow <= '0';
